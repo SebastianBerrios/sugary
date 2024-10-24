9dab4ec430a9382022f54182f945b914447bd1a1
Sebastian <sebberrios99@gmail.com>
2024-10-24 10:26:25 -0500

    add dynamic routes home

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import { createRoot } from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Layout from "./components/Layout/index.tsx";
+import Home from "./pages/Home/index.tsx";
 import "./index.css";
 
 const router = createBrowserRouter([
@@ -8,6 +9,10 @@ const router = createBrowserRouter([
     path: "/",
     element: <Layout />,
     children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
       {
         path: "categories/:categoryId",
         element: "",
