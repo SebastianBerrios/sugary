47e71f361ab1f3028b7cab03974af2a85404ec6c
Sebastian <sebberrios99@gmail.com>
2024-10-23 20:34:11 -0500

    add dynamic routes category

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,22 @@
 import { createRoot } from "react-dom/client";
-import App from "./App.tsx";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import PageHeader from "./components/PageHeader/index.tsx";
+import Category from "./components/Category/index.tsx";
 import "./index.css";
 
-createRoot(document.getElementById("root")!).render(<App />);
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <PageHeader />,
+    children: [
+      {
+        path: "categories/:categoryId",
+        element: <Category />,
+      },
+    ],
+  },
+]);
+
+createRoot(document.getElementById("root")!).render(
+  <RouterProvider router={router} />
+);
