59f3c2670198b8555df7b9ac409eeab77994df13
Sebastian <sebberrios99@gmail.com>
2024-10-23 20:38:41 -0500

    change Category to CategoryItem

---
+++ src/components/CategoryItem/index.tsx
@@ -0,0 +1,18 @@
+import { IconProp } from "@fortawesome/fontawesome-svg-core";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
+interface CategoryI {
+  label: string;
+  icon: IconProp;
+}
+
+export default function CategoryItem({ label, icon }: CategoryI) {
+  return (
+    <li className="w-40 p-1.5 border rounded-md">
+      <FontAwesomeIcon icon={icon} className="text-soft-pink pr-2" />
+      <a className="capitalize" href={`/${label}`}>
+        {label}
+      </a>
+    </li>
+  );
+}
