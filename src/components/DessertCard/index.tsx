deb7b207847f6d7c541dfbe318a031dca94476c6
Sebastian <sebberrios99@gmail.com>
2024-10-29 10:56:53 -0500

    added dessert card style and to home page

--- src/components/DessertCard/index.tsx
+++ src/components/DessertCard/index.tsx
@@ -1,4 +1,5 @@
 interface IDessertCard {
+  id: number;
   label: string;
   image: string;
   price: number;
@@ -6,20 +7,41 @@ interface IDessertCard {
 }
 
 export default function DessertCard({
+  id,
   label,
   image,
   price,
   discount = 0,
 }: IDessertCard) {
   const calcDiscount = () => {
-    return price - price;
+    return price - price * (discount / 100);
+  };
+
+  const newPrice = () => {
+    return discount > 0 ? (
+      <div className="flex gap-3">
+        <span>S/. {calcDiscount()}</span>
+        <span className="line-through text-gray-500">S/. {price}</span>
+        <span className="px-1 py-0.5 bg-red-300 text-red-500 text-xs rounded-full">
+          {discount}%
+        </span>
+      </div>
+    ) : (
+      <span>S/.{price}</span>
+    );
   };
 
   return (
-    <div>
-      <img src={image} alt={label} />
-      <p>{label}</p>
-      <p>{price}</p>
+    <div key={id} className="grid">
+      <img
+        className="size-60 rounded-xl object-cover"
+        src={image}
+        alt={label}
+      />
+      <div className="grid gap-1 pt-3 pl-2">
+        <span className="capitalize font-bold text-sm">{label}</span>
+        <span className="text-sm">{newPrice()}</span>
+      </div>
     </div>
   );
 }
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,9 +1,29 @@
+import { useContext } from "react";
 import Hero from "../../components/Hero";
+import { DessertContext } from "../../contexts/Desserts";
+import DessertCard from "../../components/DessertCard";
 
 export default function Home() {
+  const dessert = useContext(DessertContext);
+  const desserts = dessert.dessert;
+
+  console.log(desserts);
+
   return (
     <>
       <Hero />
+      <h2 className="text-center text-2xl my-3">¿Qué estas buscando hoy?</h2>
+      <section className="flex gap-5 mx-4">
+        {desserts.map((dessert: any) => (
+          <DessertCard
+            id={dessert.id}
+            label={dessert.name}
+            image={dessert.image}
+            price={dessert.price}
+            discount={dessert.discount}
+          />
+        ))}
+      </section>
     </>
   );
 }
